e14f3210b248b15aea2d0c4b7bcc322897847fa6
Laura Wilson <lgwilson75@gmail.com>
2023-06-20 19:26:49 -0500

    Create JLetter.java
    Project 1

---
+++ Code/M1/JLetter.java
@@ -0,0 +1,65 @@
+/** Prints out a J with the letters 'J' 'A' 'V' 'A'.
+ 	  	
+ * With a height and width of 10 x 12. 
+ 	  	
+ *
+ 	  	
+ * @author Laura Wilson -- Comp 1210 - 001
+ 	  	
+ *
+ 	  	
+ * @version 8/23/2020
+ 	  	
+ */
+ 	  	
+ 
+ 	  	
+ public class JLetter {
+ 	  	
+ 
+ 	  	
+ /** Prints J with JAVA letter repeatedly.
+ 	  	
+ * 
+ 	  	
+ * @param args Command line argument (not used).
+ 	  	
+ *
+ 	  	
+ */
+ 	  	
+ 
+ 	  	
+    public static void main(String[] args) {
+ 	  	
+    /** Prints out 10 x 12 J made of JAVA letter. 
+ 	  	
+    *
+ 	  	
+    */
+ 	  	
+       System.out.println("JAVAJAVAJAVA");
+ 	  	
+       System.out.println("JAVAJAVAJAVA");
+ 	  	
+       System.out.println("      JAVA  ");
+ 	  	
+       System.out.println("      JAVA  ");
+ 	  	
+       System.out.println("      JAVA  ");
+ 	  	
+       System.out.println("      JAVA  ");
+ 	  	
+       System.out.println("J     JAVA  ");
+ 	  	
+       System.out.println("JA    JAVA  ");
+ 	  	
+       System.out.println(" JAVAJAVA   ");
+ 	  	
+       System.out.println("  JAVAJA  ");
+ 	  	
+    }
+ 	  	
+ 
+ 	  	
+ }
