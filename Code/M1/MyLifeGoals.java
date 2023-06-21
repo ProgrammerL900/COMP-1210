1e7f01c8f9f09fd7592b62dd66faee65b16f0029
Laura Wilson <lgwilson75@gmail.com>
2023-06-20 19:28:16 -0500

    Create MyLifeGoals.java
    Project 1

---
+++ Code/M1/MyLifeGoals.java
@@ -0,0 +1,77 @@
+/** List of short, medium, and long goals I have.
+ 	  	
+ *
+ 	  	
+ * @author Laura Wilson -- Comp 1210 - 001
+ 	  	
+ * @version 8/23/2020 
+ 	  	
+ */
+ 	  	
+ public class MyLifeGoals {
+ 	  	
+ 
+ 	  	
+   /** Life goals for short, medium and long term goals.
+ 	  	
+    *
+ 	  	
+    * @param args Command line arguments (not used).
+ 	  	
+    */
+ 	  	
+    
+ 	  	
+    public static void main(String[] args) {
+ 	  	
+    
+ 	  	
+    /** Prints out name.
+ 	  	
+    *
+ 	  	
+    */
+ 	  	
+       System.out.println("Laura Wilson");
+ 	  	
+       System.out.println();
+ 	  	
+    
+ 	  	
+    /** Prints out short, medium and long term goals
+ 	  	
+    *
+ 	  	
+    */
+ 	  	
+       System.out.println("The short term goal that"
+ 	  	
+          + " I thought I would set for myself" 
+ 	  	
+          + " this semester, is to drink at least" 
+ 	  	
+         + " a bottle of water a day," 
+ 	  	
+          + " so that I can stay hydrated.");
+ 	  	
+       System.out.println("A medium term goal that would be" 
+ 	  	
+          + " great for me to set for myself" 
+ 	  	
+          + " would be to be able to come home"
+ 	  	
+          + " for my birthday, which is in September.");
+ 	  	
+       System.out.println("The long term goal"
+ 	  	
+          + " that I have set for myself,"
+ 	  	
+          + " now that I have achieved my previous one,"
+ 	  	
+          + " would be to actually graduate with a degree"
+ 	  	
+          + " from Auburn.");   
+ 	  	
+    } 
+ 	  	
+ }
